438b4ec805a138690a99e5065ac3d0094d7fd0f2
hiepdoai99 <nguyenxuanhiepk49@gmail.com>
2023-09-15 09:34:19 +0700

    Add existing project files to Git

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
+<script setup>
+import AppNav from "./components/NavigateBar.vue";
+import AppFooter from './components/footer.vue'
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page-container">
+    <AppNav/>
+    <div class="content-wrap">
+      <router-view></router-view>
+    </div>
+    <AppFooter/>
+  </div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page-container {
+  position: relative;
+  min-height: 100vh;
+}
+
+.content-wrap {
+  padding-bottom: 5rem;    /* Footer height */
 }
 </style>
+
